04f8037dd2dcdfdc8b61c4eb7d4d6b8729c8a73c
Chely Ho <adept_inept@yahoo.com>
2018-02-07 02:05:35 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,6 @@
 def hello_t(array)
   if block_given?
-    
+
     i = 0
 
     while i <array.length
@@ -9,6 +9,8 @@ def hello_t(array)
     end
 
     array
+  else
+    puts "Hey! No block was given!"
   end
 end
 
