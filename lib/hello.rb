7e5f035289ae4ec5a05baf4e3f97376c97ffd489
Chely Ho <adept_inept@yahoo.com>
2018-02-07 02:05:06 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,15 @@
 def hello_t(array)
-  i = 0
+  if block_given?
+    
+    i = 0
 
-  while i <array.length
-    yield(array[i])
-    i += 1
+    while i <array.length
+      yield(array[i])
+      i += 1
+    end
+
+    array
   end
-
-  array
 end
 
 # call your method here!
